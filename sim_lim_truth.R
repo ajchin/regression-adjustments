58cf19903d1eb16774c1eb0c9a41e401e7b6d632
Alex Chin <ajchin@soal-2.stanford.edu>
2018-06-06 11:53:40 -0700

    fix plot.  LR doesn't work, boo

--- sim_lim_truth.R
+++ sim_lim_truth.R
@@ -23,7 +23,7 @@ calculate_true_ATE = function(param, g, pid) {
 
 #load('data/caltech.Rdata')
 
-load('data/smallworld.Rdata')
+load('data/smallworld_large.Rdata')
 set.seed(2018)
 
 n_reps = 5000
@@ -41,15 +41,14 @@ registerDoParallel(cores=n_cores)
 # ))
 
 params = purrr::cross(list(
-  b_intercept = 0,
+  b_intercept = -0.5,
   b_direct = 1,
-  b_spill = c(0, 0.5, 1),#0.1, 0.2, 0.3, 0.4, 0.5),
+  b_spill = c(0, 0.25, 0.5, 0.75, 1),#0.1, 0.2, 0.3, 0.4, 0.5),
   max_t = c(2, 4),
   is_probit = TRUE,
   noise_sd = 1 # c(1, 3)
 ))
 
-calculate_true_ATE(params[[5]], g_fb, pid=i)
 
 print('Calculating true ATE...')
 # Calculate true ATE by simulation
@@ -57,7 +56,7 @@ true_ATE = foreach(i = 1:length(params), .combine=rbind) %do% {
   param = params[[i]]
   print(unlist(param))
   foreach(rep = 1:n_reps, .combine=rbind, .inorder=FALSE) %dopar% {
-    calculate_true_ATE(param, g_sm, pid=i)
+    calculate_true_ATE(param, g_sm_large, pid=i)
   }
 } %>% 
 data.frame %>%
@@ -65,4 +64,4 @@ group_by(pid) %>%
 summarise(ATE=mean(ATE))
 print(proc.time())
 
-write.csv(true_ATE, file='results/sim_lim/true_ATE_3.csv', row.names=FALSE)
+write.csv(true_ATE, file='results/sim_lim/true_ATE_probit.csv', row.names=FALSE)
--- sim_probit.R
+++ sim_probit.R
@@ -98,18 +98,3 @@ foreach(i = 1:length(params), .combine=rbind) %do% {
   print(proc.time())
 }
 
-
-
-# results = read.csv('results/sim_lim/results_lr.csv', header=FALSE)
-# names(results) = c('pid', 'dm', 'hajek', 'ols1', 'ols2', 'lr1', 'lr2')
-# true_ATE
-# results %>% 
-#   group_by(pid) %>% 
-#   summarise_each(funs(mean(., na.rm = TRUE)), dm, hajek, ols1, ols2, lr1, lr2) %>%
-#   left_join(true_ATE) %>% gather(k, v, dm, hajek, ols1, ols2, lr1, lr2) %>%
-#   mutate(bias_sq = (ATE - v)^2) %>% ggplot(aes(pid, bias_sq, group=k, colour=k)) + geom_point() + geom_line() + theme_bw()
-# results %>% 
-#   group_by(pid) %>% 
-#   summarise_each(funs(var(., na.rm = TRUE)), dm, hajek, ols1, ols2, lr1, lr2)
-
-# 
